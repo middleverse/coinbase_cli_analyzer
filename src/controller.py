5fbad6eb7dea42b5797cc9cbe81cdc53067a2fdb
middleverse <arshi_b@hotmail.com>
2020-11-11 10:17:32 -0500

    simplified, and added model object & delgation-to logic

--- src/controller.py
+++ src/controller.py
@@ -1,39 +1,19 @@
 import sys
 from authenticate import authenticate
-import model
+from model import AccountModel 
 
 EXIT_MARKERS = {'q', 'quit'}
-
-def findCurrency(currency):
-    return 1
-
-def displayStats(args):
-
-    # if stats for all currencies (owned now and before) are required
-    if args[0] == 'all':
-        if len(args) == 1: # display basic stats
-            return 1
-            
-    # if stats for currently owned currencies are required        
-    elif args[0] == 'curr':
-        if len(args) == 1:
-            return 2
-    else:
-        currency = findCurrency(args[0])
-        if (currency == None):
-            print('Currency stats not found. Try another currency.')
-            return 0
-        
-# returns stats for all currently held portfolio
-def getPortfolioStats(args):
-    return 1
-
+      
 # handles user input and delegation of requests
 def controller():
     # initiate authentication
     # get an auth object in return
     auth = authenticate()
 
+    # create model
+    m = AccountModel(auth)
+    m.buildModel() # TODO: make this an inherent model call?
+    
     # if program doesn't exit during authentication,
     # initiate user input
     command = ''
@@ -53,9 +33,9 @@ def controller():
         # if input is empty
         elif args[0] == 'empty':
             print('No input entered... Check README file for usage instructions.')       
-        # all other inputs (checked for validity later)
+        # all other inputs (checked for validity later) 
         else:
-            displayStats(args)        
+            m.displayStats(args)      
 
 def main():
     controller()
