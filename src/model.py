572bf796d5b3a72fc5a6b7e6106d70ac3e6cef98
middleverse <arshi_b@hotmail.com>
2020-11-17 15:35:06 -0500

    added second query delegation and skeleton functions, finished price functionality

--- src/model.py
+++ src/model.py
@@ -8,6 +8,7 @@
 # The model talks directly to the back end
 
 import json, requests
+from requests import auth
 
 API_URL = 'https://api.coinbase.com/v2/'
 API_ENDPOINTS = {
@@ -24,18 +25,22 @@ class AccountModel():
         self.auth = auth
         self.currencies_all = {}
         self.currencies_current = {}
+        self.query_currencies = []
+        self.isUSD = False
+        self.isPCT = False
+        self.buildModel()
         
     def buildModel(self):
         # get all currencies owned currently and in the past
         r = requests.get(API_URL + 'accounts', auth=self.auth)
         data = r.json()['data']
-
+        # print(data)
         # iterate through json to build currency hashtables {'name':'id'}
         for acc in data:
             if acc['updated_at'] != None: # if currency owned at any time
-                self.currencies_all[acc['name']] = acc['id']
+                self.currencies_all[acc['currency']['code']] = None
                 if float(acc['balance']['amount']) > 0: # if currency owned currently
-                    self.currencies_current[acc['name']] = acc['id']
+                    self.currencies_current[acc['currency']['code']] = None
         print('User data loaded.')
     
     def displayStats(self, args):
@@ -45,17 +50,68 @@ class AccountModel():
         # 'btc': display stats for currency name provided, ex: 'btc'
         
         # build a list of currencies to fetch data for
-        currency_list = []
         if args[0] == 'all':
-            currency_list = self.currencies_all.keys()
+            self.query_currencies = self.currencies_all.keys()
         elif args[0] == 'curr':
-            currency_list = self.currencies_current.keys()
-        else:
+            self.query_currencies = self.currencies_current.keys()
+        else:   
             # user own(s/ed) this currency
             if args[0] in self.currencies_all:
-                currency_list.append(args[0])
+                self.query_currencies.append(args[0])
             else:
                 print('User doesn\'t have a wallet with this currency, please provide a valid argument.')
-                   
-        # use second argument to route user request
+                return
 
+        # use second argument to route user request
+        if args[1] == 'price':
+            self.displaySpotPrice()
+        elif args[1] == 'owned':
+            self.displayOwned()
+        elif args[1] == 'transactions':
+            self.displayTransactions()
+        elif args[1] == 'buys':
+            self.displayBuys()
+        elif args[1] == 'sells':
+            self.displaySells()
+        elif args[1] == 'profit':
+            self.displayProfit()
+        elif args[1] == 'balance':
+            self.displayBalance()
+        else: # TODO: move this to controller
+            print('No query (second argument) provided for currency, please provide a valid argument.')
+
+    def displayOwned(self):
+        self.queryTitlePrinter('CURRENCIES OWNED:')
+        for currency in self.query_currencies:
+            print(currency)
+
+    def displaySpotPrice(self):
+        self.queryTitlePrinter('CURRENT PRICE FOR 1 UNIT OF:')
+        for currency in self.query_currencies:
+            r = requests.get(API_URL + 'prices/%s-usd/spot' % (currency), auth=self.auth)
+            data = r.json()['data']
+            print('%s: %s USD' % (currency, data['amount']))
+        return 1
+
+    def displayTransactions():
+        return 1
+
+    def displayBuys():
+        return 1
+
+    def displaySells():
+        return 1
+
+    def displayProfit():
+        return 1
+
+    def displayBalance():
+        return 1
+
+    def queryTitlePrinter(self, str):
+        print('============================')
+        print(str)
+        print('----------------------------')
+# TODO:
+# FINISH DISPLAY STATS ROUTING
+# CREATE QUERY FUNCTIONS
